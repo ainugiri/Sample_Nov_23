27c66226987bf38aa386d7e50ad10d494b63a46c
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-11-20 16:34:54 +0530

    added Sample java files

---
+++ Sample01.java
@@ -0,0 +1,7 @@
+class Sample01{
+
+	public static void main(String args[])
+	{
+		System.out.println("DevOps");
+	}
+}
---
+++ Sample02.java
@@ -0,0 +1,20 @@
+class Sample02
+{
+	public static void main(String args[])
+	{
+		System.out.println("ATOS Eviden");
+	}
+}
+
+class Test01{
+	public static void main(String args[])
+	{
+		System.out.println("Eviden");
+	}
+}
+class Test02{
+	public static void main(String args[])
+	{
+		System.out.println("welcome");
+	}
+}
